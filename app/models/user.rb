d7be5b4bdb3fd6c8196124cd87f7a27511407f34
Eric Johnson & Nick Bernard <student@epicodus.com>
2014-09-04 13:38:54 -0700

    Config UserMailer

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,15 @@
 class User < ActiveRecord::Base
-   # Include default devise modules. Others available are:
+  # Include default devise modules. Others available are:
   # :token_authenticatable, :confirmable,
   # :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
   validates_presence_of :username
+
+  after_create :send_welcome_message
+
+  def send_welcome_message
+    UserMailer.signup_confirmation(@user).deliver
+  end
 end
